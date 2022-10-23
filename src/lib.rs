9a3aca9c430911b2c7c427f105c22333562a6f36
pashutk <me@pashutk.ru>
2022-10-23 22:59:43 +0200

    Debounce input

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,8 @@ mod alloc;
 mod notes;
 mod wasm4;
 
+use std::{collections::HashMap, ops::Deref};
+
 use notes::{note_freq, note_from_string, note_to_render};
 use wasm4::*;
 
@@ -86,21 +88,91 @@ impl Tracker {
 
 static mut TRACKER: Tracker = Tracker::empty();
 
+static mut TICK: u32 = 0;
+
+static mut LAST_CALL: u32 = 0;
+
+static mut TIMERS: Timers = Timers {
+    tick: 0,
+    last_calls: None,
+};
+
+struct Timers {
+    tick: u32,
+    last_calls: Option<HashMap<String, u32>>,
+}
+
+impl Timers {
+    fn init(&mut self) {
+        self.last_calls = Some(HashMap::new());
+    }
+
+    fn tick(&mut self) {
+        self.tick += 1;
+    }
+
+    fn run_action<F>(&mut self, key: String, action: F)
+    where
+        F: FnOnce(),
+    {
+        let map = self
+            .last_calls
+            .as_mut()
+            .expect("Timers should be initialized");
+        map.insert(key, self.tick);
+        action()
+    }
+
+    fn run_action_debounced<F>(&mut self, key: String, t: u32, action: F)
+    where
+        F: FnOnce(),
+    {
+        let map = self
+            .last_calls
+            .as_ref()
+            .expect("Timers should be initialized");
+        let last_call = map.get(&key).map_or(0, |a| *a);
+        if self.tick - last_call > t {
+            self.run_action(key, action)
+        }
+    }
+}
+
+enum Color {
+    Background,
+    Light,
+    Primary,
+    Dark,
+}
+
+fn set_color(color: Color) {
+    unsafe {
+        *DRAW_COLORS = match color {
+            Color::Background => 1,
+            Color::Light => 2,
+            Color::Primary => 3,
+            Color::Dark => 4,
+        }
+    }
+}
+
 #[no_mangle]
 fn start() {
-    unsafe { TRACKER = Tracker::new() }
+    unsafe {
+        TRACKER = Tracker::new();
+        TIMERS.init();
+    }
 }
 
 #[no_mangle]
 fn update() {
-    unsafe { *DRAW_COLORS = 3 }
+    set_color(Color::Primary);
 
     let cursor: u8;
     unsafe { cursor = TRACKER.cursor_tick };
 
     for line in 0..16 {
         text(format!("{:0X}", line), 1, line * 10 + 1);
-        // let cursor: &u8 = &TRACKER.cursor_tick.to_owned();
         let note: Option<usize>;
         unsafe {
             note = TRACKER.pattern[line as usize];
@@ -113,9 +185,9 @@ fn update() {
 
         if line == cursor.into() {
             rect(15, line * 10, 26, 10);
-            unsafe { *DRAW_COLORS = 1 }
+            set_color(Color::Background);
             text(name, 16, line * 10 + 1);
-            unsafe { *DRAW_COLORS = 3 }
+            set_color(Color::Primary);
         } else {
             text(name, 16, line * 10 + 1);
         };
@@ -125,31 +197,42 @@ fn update() {
     if gamepad & BUTTON_1 != 0 {
         if gamepad & BUTTON_RIGHT != 0 {
             unsafe {
-                if let Some(note) = TRACKER.pattern[cursor as usize] {
-                    if note < note_freq.len() - 1 {
-                        TRACKER.pattern[cursor as usize] = Some(note + 1)
+                TIMERS.run_action_debounced("pitch_up".to_string(), 4, || {
+                    if let Some(note) = TRACKER.pattern[cursor as usize] {
+                        if note < note_freq.len() - 1 {
+                            TRACKER.pattern[cursor as usize] = Some(note + 1)
+                        }
                     }
-                }
+                })
             }
         } else if gamepad & BUTTON_LEFT != 0 {
             unsafe {
-                if let Some(note) = TRACKER.pattern[cursor as usize] {
-                    if note != 0 {
-                        TRACKER.pattern[cursor as usize] = Some(note - 1)
+                TIMERS.run_action_debounced("pitch_down".to_string(), 4, || {
+                    if let Some(note) = TRACKER.pattern[cursor as usize] {
+                        if note != 0 {
+                            TRACKER.pattern[cursor as usize] = Some(note - 1)
+                        }
                     }
-                }
+                })
             }
         }
     } else if gamepad & BUTTON_DOWN != 0 && cursor < 15 {
-        unsafe { TRACKER.cursor_tick = cursor + 1 }
+        unsafe {
+            TIMERS.run_action_debounced("nav_down".to_string(), 4, || {
+                TRACKER.cursor_tick = cursor + 1
+            })
+        }
     } else if gamepad & BUTTON_UP != 0 && cursor != 0 {
-        unsafe { TRACKER.cursor_tick = cursor - 1 }
+        unsafe {
+            TIMERS
+                .run_action_debounced("nav_up".to_string(), 4, || TRACKER.cursor_tick = cursor - 1)
+        }
     }
 
-    unsafe { *DRAW_COLORS = 2 }
+    set_color(Color::Light);
     text("nav:   arrows", 46, 50);
     text("pitch: X+L/R", 46, 60);
-    unsafe { *DRAW_COLORS = 3 }
+    set_color(Color::Primary);
 
     unsafe {
         let tick: i32 = TRACKER.tick.into();
@@ -157,4 +240,5 @@ fn update() {
 
         TRACKER.update()
     }
+    unsafe { TIMERS.tick() }
 }
